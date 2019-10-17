aa2411c33b06de38021deb841e46573b556a7428
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-10-17 15:21:57 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan31-perkenalanmahasiswa/src/pboulang/pkg10115109/latihan31/perkenalanmahasiswa/Pboulang10115109Latihan31Perkenalanmahasiswa.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author nama : indri yuliyanti
+ *          nim : 10115109
+ *          kls : pbo ulang
+ *          tugas pert 3 : perkenalan mahasiswa
+ */
+public class Pboulang10115109Latihan31Perkenalanmahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello everyone ");  
+        System.out.println("My NIM \t\t: 10115109");
+        System.out.println("MY Name\t\t: Indri yuliyanti"+"\n"+"");
+        
+        
+        System.out.println("hello everyone ");
+        System.out.println("My NIM \t\t: 10110270");
+        System.out.println("MY Name\t\t: Indra tiola"+"\n"+"");
+        
+        System.out.println("hello everyone ");
+        System.out.println("My NIM \t\t: 10110271");
+        System.out.println("MY Name\t\t: robo tanzil"+"\n"+"");
+        
+        System.out.println("hello everyone ");
+        System.out.println("My NIM \t\t: 10110269");
+        System.out.println("MY Name\t\t: muhamad nur awaludin"+"\n"+"");
+    }
+    
+}
